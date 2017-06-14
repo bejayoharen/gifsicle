1f56ca0792ae075fc5f749a4286244ceaf90d264
Bjorn Roche <bjorn@xowave.com>
2017-06-14 17:07:49 -0400

    s/gifsicle.dylib/libgifsicle.dylib

--- compile-dylib.sh
+++ compile-dylib.sh
@@ -2,6 +2,7 @@ autoreconf -i
 ./configure
 make CFLAGS="-fvisibility=hidden -DGIFSICLE_DLL" gifsicle
 cd src
-gcc -W -Wall -fvisibility=hidden -dynamiclib -o gifsicle.dylib clp.o fmalloc.o giffunc.o gifread.o gifunopt.o merge.o optimize.o quantize.o support.o xform.o gifsicle.o  gifwrite.o
+gcc -W -Wall -fvisibility=hidden -dynamiclib -install_name libgifsicle.dylib -o libgifsicle.dylib clp.o fmalloc.o giffunc.o gifread.o gifunopt.o merge.o optimize.o quantize.o support.o xform.o gifsicle.o  gifwrite.o
 cd ..
-nm -gU src/gifsicle
+nm -gU src/libgifsicle.dylib
+otool -D src/libgifsicle.dylib
