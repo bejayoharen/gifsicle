fbc9a1e8e20c627a86a58ab7869a7f8a8ed5ceee
Bjorn Roche <bjorn@xowave.com>
2017-06-23 11:44:15 -0400

    tweaks and docs

--- compile-dylib.sh
+++ compile-dylib.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 set -e
 
+
+if [ "$1" == "true" ] ; then
+	echo "debugging..."
+	CFLAGS="-fvisibility=hidden -DTRACE_LOGGING -DGIFSICLE_DLL"
+elif [ "$1" == "false" ] ; then
+	echo "Not debugging..."
+	CFLAGS="-fvisibility=hidden -DGIFSICLE_DLL"
+else
+	echo
+	echo "Usage: $0 true|false"
+	echo "    first argument signifies if extra trace debugging should be used."
+	echo
+	exit 1
+fi
+
 autoreconf -i
 ./configure
 make clean
-make CFLAGS="-fvisibility=hidden -DTRACE_LOGGING -DGIFSICLE_DLL" gifsicle
+make CFLAGS="$CFLAGS" gifsicle
 cd src
 gcc -W -Wall -fvisibility=hidden -dynamiclib -install_name libgifsicle.dylib -o libgifsicle.dylib clp.o fmalloc.o giffunc.o gifread.o gifunopt.o merge.o optimize.o quantize.o support.o xform.o gifsicle.o  gifwrite.o
 cd ..
